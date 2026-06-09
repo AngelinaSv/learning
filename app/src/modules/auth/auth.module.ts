bfe60c876769fd77eae2ed7646ea07e690bda164
AngelinaSv <anna97asakura@gmail.com>
2026-06-09 15:56:33 +0300

    refactor(auth): simplify authentication flow

--- app/src/modules/auth/auth.module.ts
+++ app/src/modules/auth/auth.module.ts
@@ -5,11 +5,10 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport/dist/passport.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { SecurityModule } from 'src/common/security/security.module';
-import { AuthCookieService } from './auth-cookie.service';
 
 @Module({
   imports: [UsersModule, PassportModule, SecurityModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, AuthCookieService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
